5130987d7080d8b8dad525ee392a21bfb0dd2cc9
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-03-20 01:51:34 +0300

    fixed paths

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import HomePage from "./pages/HomePage";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/HomePage";
 import AboutMe from "./pages/AboutMe";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-import "./styles/App.css"
+import "./styles/App.css";
 
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
-
 function App() {
   return (
     <div>
-        <Router basename="/React-Portfolio">
-          <Navbar />
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/about" component={AboutMe} />
-            <Route exact path="/portfolio" component={Portfolio} />
-            <Route exact path="/contact" component={Contact} />
-        </Router>
-        <Footer />
+      <Router Basename="https://github.com/Abduler21/React-Portfolio">
+        <Navbar />
+
+        <Route exact path="/Abduler21/React-Portfolio/" component={Home} />
+        <Route
+          exact
+          path="/Abduler21/React-Portfolio/about"
+          component={AboutMe}
+        />
+        <Route
+          exact
+          path="/Abduler21/React-Portfolio/portfolio"
+          component={Portfolio}
+        />
+        <Route
+          exact
+          path="/Abduler21/React-Portfolio/contact"
+          component={Contact}
+        />
+      </Router>
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
